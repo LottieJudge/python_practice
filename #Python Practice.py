780f4b881c9c400721e9bea3b4fd9fa07aa5de0d
cactusballs <lottielucyjudge@gmail.com>
2025-01-13 14:55:51 +0000

    Python revision

---
+++ #Python Practice.py
@@ -0,0 +1,35 @@
+# in Python instead of using console.log() we use print()
+#Can still use single or double quotes 
+
+print("Hello World!")
+
+#Variables do not need var, const or let. Just name them and then assign the data 
+#rememember to capitalise booleans 
+
+my_name = "Lottie"
+my_age = 31
+a_member = False
+print(a_member)
+
+#to update a variable you just reassign 
+
+a_member = True
+print(a_member)
+
+#Errors
+
+syntax_error = "an error when you've written something wrong, such as mismatched quotes or a missing bracket"
+
+name_error = "trying to print an undefined variable for example"
+
+#Numbers 
+
+an_int = 2 #a whole num
+a_float = 10.5 #a decimal point 
+
+print(an_int + 3)
+print(a_float / 2)
+
+
+
+
