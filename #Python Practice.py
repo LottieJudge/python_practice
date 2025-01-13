3891a6ec90bb1ace1d2d24e928d8067746fa5455
cactusballs <lottielucyjudge@gmail.com>
2025-01-13 18:29:37 +0000

    sorting out DS store issues

--- #Python Practice.py
+++ #Python Practice.py
@@ -9,12 +9,12 @@ print("Hello World!")
 my_name = "Lottie"
 my_age = 31
 a_member = False
-print(a_member)
+#print(a_member)
 
 #to update a variable you just reassign 
 
 a_member = True
-print(a_member)
+#print(a_member)
 
 #Errors
 
@@ -27,9 +27,39 @@ name_error = "trying to print an undefined variable for example"
 an_int = 2 #a whole num
 a_float = 10.5 #a decimal point 
 
-print(an_int + 3)
-print(a_float / 2)
+# print(an_int + 3)
+# print(a_float / 2)
 
+#performing calculations with python (much like performing calculations with any language )
 
+add = 1+1
 
+minus = 2-1
+
+times = 4*4
+
+division = 70/4
+
+modulo = 90 % 7 
+
+exponentiation = 2**3
+
+#concatenation, joining to variables together 
+
+greeting = "hello"
+name = "Yoda"
+
+sign_in_text = greeting + ' ' + name
+
+#print(sign_in_text)
+
+#plus equals, for if you have a variable you need to update eg adding another number 
+
+number_of_miles_hiked = 12
+
+number_of_miles_hiked += 2 
+
+#print(number_of_miles_hiked) - prints out 14
+
+#multi line strings 
 
