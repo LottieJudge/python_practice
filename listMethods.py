5c947909ae12c6f36fd660454dc069c90e10c212
cactusballs <lottielucyjudge@gmail.com>
2025-01-19 17:09:29 +0000

    range

--- listMethods.py
+++ listMethods.py
@@ -2,8 +2,8 @@
 count = ".count() - a list method to count the number of times an element occurs in a list"
 insert = ".insert() - a list method to insert an element at a specified index"
 pop = ".pop() - a list method to remove an element at the end of the list or at a specified index"
-range = ".range() - a built in function to create a sequence of integers"
-len = "A built in Python function to get the length of a list"
+range_func = ".range() - a built in function to create a sequence of integers"
+len_func = "A built in Python function to get the length of a list"
 sort_sorted = "A method and function to sort a list"
 
 fave_birds = ["Humble Rock Dove", "Robin", "Osprey", "Hen Harrier"]
@@ -18,3 +18,7 @@ print(fave_birds)
 remove_wrong_bird = fave_birds.pop(3)
 print(remove_wrong_bird)
 
+#Range - The function range() takes a single input, and generates numbers starting at 0 and ending at the number before the input.So, if we want the numbers from 0 through 9, we use range(10) because 10 is 1 greater than 9:
+
+how_good_are_birds = range(11)
+print(list(how_good_are_birds))
