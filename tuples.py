a6b01919293a427976ca79a97a69a05edf9167e1
cactusballs <lottielucyjudge@gmail.com>
2025-01-21 18:19:27 +0000

    tuples

---
+++ tuples.py
@@ -0,0 +1,7 @@
+#tuples are immutable meaning they can't be changed but they are more energy efficient in both time and memory (and money baby) - good to use if working with data that doesn't ever need to be changed. A tuple is defined by a variable and then normal brackets (). Tuples can also hold just one item as long as it's followed by a comma 
+
+popcorn_flavours = ("Sweet", "Salty", "Cinnamon", "Butter")
+
+#indexing works the same as lists 
+print(popcorn_flavours[0])
+
