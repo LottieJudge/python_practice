94494eac61fbb29277bc0a9164f1bcdf94734eca
cactusballs <lottielucyjudge@gmail.com>
2025-01-22 08:54:38 +0000

    loooops

---
+++ loops.py
@@ -0,0 +1,6 @@
+#Loops are used for repeating tasks in code. E.G looping through a playlist on spotify, or given a specific pin and email looping through customer database to find the matches and returning that info 
+
+playlist = ["English Rose", "Drop Seven", "Worms"]
+for song in playlist:
+  print(f"now playing {song}")
+
