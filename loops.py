9180b78fe00189af9b04d695d197960e89c4246d
cactusballs <lottielucyjudge@gmail.com>
2025-01-23 18:50:18 +0000

    range in loops

--- loops.py
+++ loops.py
@@ -17,3 +17,11 @@ print(playlist[2])
 
 #the temp variable is i in JS, but can be called anything, it's just used as an instance to iterate through. The Data structure could be a list, dictionary or tuple. and then perform a function. EG append(), pop() or just return it baby 
 
+#loops using range - performing the function a set numbe rof times 
+
+six_times = range(6)
+
+for num in range(6):
+  print("Loop number:" + str(num +1))
+
+
