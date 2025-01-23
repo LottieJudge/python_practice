79ff0a37a98c6b90f313cef1f4cf5e692ce2c6f5
cactusballs <lottielucyjudge@gmail.com>
2025-01-23 07:08:05 +0000

    commit

--- loops.py
+++ loops.py
@@ -4,3 +4,16 @@ playlist = ["English Rose", "Drop Seven", "Worms"]
 for song in playlist:
   print(f"now playing {song}")
 
+#without a loop we would have to print the above using indexing - long lads, long! 
+
+print(playlist[0])
+print(playlist[1])
+print(playlist[2])
+
+#blue print of a loop 
+
+#for temporary_variable in data_structure:
+  #print(perform_action)
+
+#the temp variable is i in JS, but can be called anything, it's just used as an instance to iterate through. The Data structure could be a list, dictionary or tuple. and then perform a function. EG append(), pop() or just return it baby 
+
