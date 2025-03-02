1b7abb1bcf264d6188e592e764f7cd1fd1f072f0
cactusballs <lottielucyjudge@gmail.com>
2025-03-02 22:11:06 +0000

    leet coding

--- leetCode.py
+++ leetCode.py
@@ -1,7 +1,20 @@
 #flowers
-#0 = empty and one means not empty, flower cannot be planted next to another flower. if a flower can be planted the function should return true, if not should return false 
+#0 = empty and one means not empty, flower cannot be planted next to another flower. if a flower can be planted the function should return true, if not should return false. 
 #need a return statmemnet of boolean true or false and 
 
-class Solution(object):
-    def canPlaceFlowers(self, flowerbed, n):
-       
+# true_flowerbed = 1
+# false_flowerbed = 1
+# flowerbed = [1, 0, 0, 0, 1]
+
+def canPlaceFlowers(flowerbed, n):
+      count_zero = flowerbed.count(0)
+      count_one = flowerbed.count(1)
+      answer = ''
+      if(count_zero > count_one) :
+        answer = True
+      else:
+        answer = False
+      return answer
+
+counted = canPlaceFlowers([1, 0, 1, 0, 1, 0, 0], 1)
+print(counted)
