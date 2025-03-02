4403db1f0f3cb8098f905b0d185c39af6cbaa2d5
cactusballs <lottielucyjudge@gmail.com>
2025-03-02 22:16:31 +0000

    flower bed

--- leetCode.py
+++ leetCode.py
@@ -10,11 +10,11 @@ def canPlaceFlowers(flowerbed, n):
       count_zero = flowerbed.count(0)
       count_one = flowerbed.count(1)
       answer = ''
-      if(count_zero > count_one) :
+      if(count_zero > count_one) and (count_zero > n + 1):
         answer = True
       else:
         answer = False
       return answer
 
-counted = canPlaceFlowers([1, 0, 1, 0, 1, 0, 0], 1)
+counted = canPlaceFlowers([1, 0, 0, 0, 1], 1)
 print(counted)
