7b8747a195b7b4770b98d3dd1bb496a2a507da81
cactusballs <lottielucyjudge@gmail.com>
2025-03-02 21:53:58 +0000

    starting leet codes

---
+++ leetCode.py
@@ -0,0 +1,7 @@
+#flowers
+#0 = empty and one means not empty, flower cannot be planted next to another flower. if a flower can be planted the function should return true, if not should return false 
+#need a return statmemnet of boolean true or false and 
+
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+       
