be665d075d835812f9f0eba9783e9ac6e30b380d
cactusballs <lottielucyjudge@gmail.com>
2025-03-16 11:08:15 +0000

    Longest Word Python start

--- longestW.py
+++ longestW.py
@@ -1,3 +1,9 @@
+import re
 
 def LongestWord(sen):
-  senList = sen.
+  senList = sen.split(" ")
+  for word in senList:
+    wordNoChars = re.sub(r"[^a-zA-Z]"," ", word)
+    newList = senList.append(wordNoChars)
+          
+print(LongestWord("fun&!! time"))
