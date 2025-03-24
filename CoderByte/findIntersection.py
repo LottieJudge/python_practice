cc033f3dd39a9ddd2482793a5ecc99378ee76e66
lottiejudge <lottielucyjudge@gmail.com>
2025-03-24 17:48:29 +0000

    newCoderByte

--- CoderByte/findIntersection.py
+++ CoderByte/findIntersection.py
@@ -1,14 +1,14 @@
 def FindIntersection(strArr):
-  wholeString = " ".join(sorted(strArr))
-  print(wholeString)
-  newString = ""
-  for char in set(wholeString):
-    if  char.isdigit() and wholeString.count(char) > 1:
-      newString += f"{char}, "
-      
-  return newString
+  strArr = (", ").join(strArr)
+  strArr =  strArr.split(",")
+  print(strArr)
+  nums = []
+  for num in set(strArr):
+      if strArr.count(num) > 1:
+       nums.append(num)
+      #  print(nums)
+
   
-
-
 # keep this function call here 
 print(FindIntersection(["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]));
+
---
+++ CoderByte/questionMarks.py
@@ -0,0 +1,7 @@
+def QuestionsMarks(strParam):
+  
+  # code goes here
+  return strParam
+
+# keep this function call here 
+print(QuestionsMarks('aa6?9'))
