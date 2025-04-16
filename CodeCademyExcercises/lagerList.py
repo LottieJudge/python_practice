b078ed615efc6e0f1771abc95d967887f36e20bd
lottiejudge <lottielucyjudge@gmail.com>
2025-04-16 19:08:36 +0100

    larger (or lager) list using if and else

--- CodeCademyExcercises/lagerList.py
+++ CodeCademyExcercises/lagerList.py
@@ -1,3 +1,13 @@
 # Write a function named larger_list() that has two parameters named my_list1 and my_list2. The function should return the last element of the list that contains more elements. If both lists are the same size, then return the last element of my_list1.
 
-def larger_list
+def larger_list(my_list1, my_list2):
+    list1len = len(my_list1)
+    list2len = len(my_list2)
+    if list1len == list2len:
+        return my_list1[-1]
+    if list1len > list2len:
+        return my_list1[-1]
+    else:
+        return my_list2[-1]
+    
+print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
