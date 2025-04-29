fadb06fba5fcd3cf5a8b15f6ded84914cf22f041
lottiejudge <lottielucyjudge@gmail.com>
2025-04-29 20:12:12 +0100

    practicing

--- CodeCademyExcercises/advanced_loops/over9000.py
+++ CodeCademyExcercises/advanced_loops/over9000.py
@@ -1,4 +1,8 @@
 def over_nine_thousand(lst):
-  sum = 0 
-  for num in lst:
-    
+  num = 0 
+  while num <= 9000:
+    for int in lst:
+      num += int
+  return num
+  
+print(over_nine_thousand([8000, 900, 120, 5000]))
