c876ef8ef2a3c515499eeed66df37cf3a73878f4
lottiejudge <lottielucyjudge@gmail.com>
2025-04-29 21:17:18 +0100

    got it !

--- CodeCademyExcercises/advanced_loops/over9000.py
+++ CodeCademyExcercises/advanced_loops/over9000.py
@@ -1,8 +1,9 @@
 def over_nine_thousand(lst):
   num = 0
   for int in lst:
-     while num <= 9000:
       num += int
+      if num >= 9000:
+         break
   return num
   
 print(over_nine_thousand([8000, 900, 120, 5000]))
