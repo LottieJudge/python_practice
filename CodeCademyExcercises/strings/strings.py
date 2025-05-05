5d8c6261385b24101b428ed3509e81b0a61dbf4e
lottiejudge <lottielucyjudge@gmail.com>
2025-05-05 10:06:21 +0100

    using \n to split

--- CodeCademyExcercises/strings/strings.py
+++ CodeCademyExcercises/strings/strings.py
@@ -77,3 +77,25 @@ print(name_lower, name_title, name_upper)
 sentence = "I love dogs"
 sentence_split = sentence.split(' ')
 print(sentence_split)
+
+# escape sequences \n - new line \t - horizontal tab
+
+spring_storm_text = \
+"""The sky has given over 
+its bitterness. 
+Out of the dark change 
+all day long 
+rain falls and falls 
+as if it would never end. 
+Still the snow keeps 
+its hold on the ground. 
+But water, water 
+from a thousand runnels! 
+It collects swiftly, 
+dappled with black 
+cuts a way for itself 
+through green ice in the gutters. 
+Drop after drop it falls 
+from the withered grass-stems 
+of the overhanging embankment."""
+
