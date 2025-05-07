4aa902dedb8e99ff322bed4e19109a3601447859
lottiejudge <lottielucyjudge@gmail.com>
2025-05-07 19:17:22 +0100

    diception

--- CodeCademyExcercises/dictionaries/dictionaries.py
+++ CodeCademyExcercises/dictionaries/dictionaries.py
@@ -2,3 +2,7 @@
 
 menu = {"Latte":3, "Flat White": 3.5, "Tea": 1.5, "Filter": 1}
 print(menu)
+
+# dictionaries can be any data type - even more dictionaries!!! 
+books_i_like = {"Nature":["The Golden Mole", "English Pastorial", "Britains Rainforests"], "Fantasy":["Wee Free Men", "Rivers of London", "Dune"]}
+print(books_i_like)
