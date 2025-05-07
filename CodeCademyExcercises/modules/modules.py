15a567b18d03036bc77da8679434fb8112e7a37d
lottiejudge <lottielucyjudge@gmail.com>
2025-05-07 17:09:30 +0100

    decimal module cont

--- CodeCademyExcercises/modules/modules.py
+++ CodeCademyExcercises/modules/modules.py
@@ -16,3 +16,9 @@ print(randomer_number)
 
 from decimal import Decimal 
 
+cost_of_gum = Decimal('0.10')
+cost_of_pop = Decimal('1.35')
+total_cost = cost_of_gum + cost_of_pop
+print(total_cost)
+print(0.10 + 1.35)
+# big difference using decimal but I wonder if theres a more built in method without importing from a module?
