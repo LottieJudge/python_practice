0a7093eb952da384433530ee684f738ff4dbeaa1
lottiejudge <lottielucyjudge@gmail.com>
2025-05-07 18:51:00 +0100

    starting dictionaries

---
+++ CodeCademyExcercises/dictionaries/dictionaries.py
@@ -0,0 +1,4 @@
+# a dictionary is an unordered set of key: value pairs (key = key : value = the data lads) its a way of mapping data so that we can find values quickly that are associated to one another e.g 
+
+menu = {"Latte":3, "Flat White": 3.5, "Tea": 1.5, "Filter": 1}
+print(menu)
