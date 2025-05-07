e2b81be075228feff21ab638d1ef3dfeebd345d5
lottiejudge <lottielucyjudge@gmail.com>
2025-05-07 18:46:18 +0100

    next module

--- CodeCademyExcercises/modules/timtravel/time_travelers_toolkit.py
+++ CodeCademyExcercises/modules/timtravel/time_travelers_toolkit.py
@@ -4,3 +4,7 @@ from random import randint, choice
 
 current_date = dt.now()
 print(f"You plane to travel on {current_date}")
+base_cost = Decimal('100.00')
+year_difference = abs(current_year - target_year)
+
+final_cost = round(base_cost, 2)
