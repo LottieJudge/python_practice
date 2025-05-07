659e3eb5f07d567030ed345957005c67373ade74
lottiejudge <lottielucyjudge@gmail.com>
2025-05-07 18:24:14 +0100

    creating module

--- CodeCademyExcercises/modules/timtravel/custom_module.py
+++ CodeCademyExcercises/modules/timtravel/custom_module.py
@@ -1,2 +1,3 @@
 def generate_time_travel_message(destination, year, cost):
   return 'Your trip to {destination} in {year} will cost: £{cost}'.format(destination = destination, year = year, cost=cost)
+
---
+++ CodeCademyExcercises/modules/timtravel/time_travelers_toolkit.py
@@ -0,0 +1,6 @@
+from datetime import datetime as dt 
+from decimal import Decimal 
+from random import randint, choice
+
+current_date = dt.now()
+print(f"You plane to travel on {current_date}")
