f488945801d63ede86c375bf84678971937e9f52
lottiejudge <lottielucyjudge@gmail.com>
2025-05-08 18:13:23 +0100

    values()

--- CodeCademyExcercises/dictionaries/dictionaries.py
+++ CodeCademyExcercises/dictionaries/dictionaries.py
@@ -56,4 +56,10 @@ lessons = num_exercises.keys()
 print(users)
 print(lessons)
 
+# get all values, using values() accesses the value in the key value pair, meaning you could add everything 
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
 
+total_exercises = 0 
+for x in num_exercises.values():
+  num_exercises += x
+print(total_exercises)
