cad4afc238a0015fa909d15245e00b511864571f
lottiejudge <lottielucyjudge@gmail.com>
2025-05-08 18:26:49 +0100

    end of module exercise

--- CodeCademyExcercises/dictionaries/dictionaries.py
+++ CodeCademyExcercises/dictionaries/dictionaries.py
@@ -74,3 +74,14 @@ for genre, value in books_i_like.items():
 pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58, "Physician": 40, "Lawyer": 37, "Aerospace Engineer": 9}
 for title, value in pct_women_in_occupation.items():
   print(f"Women make up {value} percent of {title}s.")
+
+  # end of module exercise 
+
+tarot = { 1:	"The Magician", 2:	"The High Priestess", 3:	"The Empress", 4:	"The Emperor", 5:	"The Hierophant", 6:	"The Lovers", 7:	"The Chariot", 8:	"Strength", 9:	"The Hermit", 10:	"Wheel of Fortune", 11:	"Justice", 12:	"The Hanged Man", 13:	"Death", 14:	"Temperance", 15:	"The Devil", 16:	"The Tower", 17:	"The Star", 18:	"The Moon", 19:	"The Sun", 20:	"Judgement", 21:	"The World", 22: "The Fool"}
+spread = {}
+spread["past"] = tarot.pop(13)
+spread["present"] = tarot.pop(22)
+spread["future"] = tarot.pop(10)
+
+for key, value in spread.items():
+  print(f"Your {key} is the {value} card.")
