dd74bdaafd99209ca219a64cbf0d123ba678eda3
lottiejudge <lottielucyjudge@gmail.com>
2025-05-08 19:39:04 +0100

    setting up the function

--- CodeCademyExcercises/dictionaries/scrabble.py
+++ CodeCademyExcercises/dictionaries/scrabble.py
@@ -3,3 +3,12 @@ points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 
 
 letter_to_points = {key:value for key, value in zip(letters, points)}
 letter_to_points[" "] = 0
+
+
+def score_word(word):
+  point_total = 0
+  for x in word:
+    point_total += letter_to_points.get(x,0)
+  return point_total
+
+print(score_word("pal"))
