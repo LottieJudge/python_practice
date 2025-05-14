d9a5794505db2ee8fe015aace8efc32f43ecb2d9
lottiejudge <lottielucyjudge@gmail.com>
2025-05-14 18:13:50 +0100

    no blank lines

--- CodeCademyExcercises/learningFiles/file.py
+++ CodeCademyExcercises/learningFiles/file.py
@@ -5,3 +5,9 @@ with open("/Users/lottiejudge/Desktop/codingFiles/python_practice/CodeCademyExce
 with open("/Users/lottiejudge/Desktop/codingFiles/python_practice/CodeCademyExcercises/learningFiles/poem.txt") as poem_file:
   for line in poem_file.readlines():
     print(line)
+
+
+with open("/Users/lottiejudge/Desktop/codingFiles/python_practice/CodeCademyExcercises/learningFiles/poem.txt") as poem_byLine:
+  first_line = poem_byLine.readline()
+  second_line = poem_byLine.readline()
+  print(second_line)
