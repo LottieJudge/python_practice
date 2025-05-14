27a41aa6095a950ee5438a4321caf9dbb573d154
lottiejudge <lottielucyjudge@gmail.com>
2025-05-14 17:59:07 +0100

    yesssss

--- CodeCademyExcercises/learningFiles/file.py
+++ CodeCademyExcercises/learningFiles/file.py
@@ -1,4 +1,4 @@
 with open("/Users/lottiejudge/Desktop/codingFiles/python_practice/CodeCademyExcercises/learningFiles/python.txt") as text_file:
   text_data = text_file.read()
 
-print(text_file)
+print(text_data)
