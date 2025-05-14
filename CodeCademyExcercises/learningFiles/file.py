f32b2598598db1f81abd375b1b4beec6b1997d0b
lottiejudge <lottielucyjudge@gmail.com>
2025-05-14 18:04:05 +0100

    heck yeah!!

--- CodeCademyExcercises/learningFiles/file.py
+++ CodeCademyExcercises/learningFiles/file.py
@@ -2,3 +2,6 @@ with open("/Users/lottiejudge/Desktop/codingFiles/python_practice/CodeCademyExce
   text_data = text_file.read()
 
 # print(text_data)
+with open("/Users/lottiejudge/Desktop/codingFiles/python_practice/CodeCademyExcercises/learningFiles/poem.txt") as poem_file:
+  for line in poem_file.readlines():
+    print(line)
