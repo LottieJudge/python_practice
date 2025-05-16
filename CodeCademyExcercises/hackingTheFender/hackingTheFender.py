cfd936a970ff2e1460b6b42af96e1236d9862703
lottiejudge <lottielucyjudge@gmail.com>
2025-05-16 13:48:56 +0100

    hacking the fender

--- CodeCademyExcercises/hackingTheFender/hackingTheFender.py
+++ CodeCademyExcercises/hackingTheFender/hackingTheFender.py
@@ -2,9 +2,10 @@ import csv
 
 compromised_users = []
 
-with open("CodeCademyExcercises/hackingTheFender/passwords.csv") as password_file:
+with open("passwords.csv") as password_file:
   password_csv = csv.DictReader(password_file)
   for line in password_csv:
     password_row = []
     password_row.append(line)
-    print(password_row[0]['Username'])
+    compromised_users.append(password_row[0]['Username'])
+
