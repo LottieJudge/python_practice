281512a19f5d312896dbdaa7ead1e2f8a5a0ff08
lottiejudge <lottielucyjudge@gmail.com>
2025-05-16 08:29:13 +0100

    adding in indexing for the loop to parse correctly

--- CodeCademyExcercises/hackingTheFender/hackingTheFender.py
+++ CodeCademyExcercises/hackingTheFender/hackingTheFender.py
@@ -2,6 +2,9 @@ import csv
 
 compromised_users = []
 
-with open("passwords.csv") as password_file:
-  password_csv = csv.DictReader
-  
+with open("CodeCademyExcercises/hackingTheFender/passwords.csv") as password_file:
+  password_csv = csv.DictReader(password_file)
+  for line in password_csv:
+    password_row = []
+    password_row.append(line)
+    print(password_row[0]['Username'])
