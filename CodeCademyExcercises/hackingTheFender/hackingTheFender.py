b7caafe422c98aa763529ab33b72f3fdf8c54ffb
lottiejudge <lottielucyjudge@gmail.com>
2025-05-19 19:13:29 +0100

    json part

--- CodeCademyExcercises/hackingTheFender/hackingTheFender.py
+++ CodeCademyExcercises/hackingTheFender/hackingTheFender.py
@@ -13,3 +13,9 @@ with open("CodeCademyExcercises/hackingTheFender/passwords.csv") as password_fil
 with open('compromised_users.txt', "w") as compromised_user_file:
   for user in compromised_users:
     compromised_user_file.write(f"Username:{user}.\n ")
+
+with open('boss-message.json', "w") as boss_message:
+  boss_message_dict = {
+    "recipient" : "The Boss",
+    "message" : "Mission Success"
+  }
