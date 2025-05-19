0bdc4ec8a6154e12ef85bab8c1a368ef1bdfeb4b
lottiejudge <lottielucyjudge@gmail.com>
2025-05-19 21:25:10 +0100

    using type() to know which class a data set is (type of data)

--- CodeCademyExcercises/classes/types.py
+++ CodeCademyExcercises/classes/types.py
@@ -1,1 +1,9 @@
-# Type - to check the type of data stored in 
+# Type - to check the type of data stored in a variable etc we use type()
+
+my_string = 'Watson and Chewie are the best!'
+my_number = 69
+my_dict = {}
+
+print(type(my_string))
+print(type(my_number))
+print(type(my_dict))
