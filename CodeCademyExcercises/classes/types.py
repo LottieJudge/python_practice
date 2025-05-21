62fd869dfd8e00b28c8ab02f03d73c30d257e878
lottiejudge <lottielucyjudge@gmail.com>
2025-05-21 15:11:12 +0100

    classes

--- CodeCademyExcercises/classes/types.py
+++ CodeCademyExcercises/classes/types.py
@@ -7,3 +7,9 @@ my_dict = {}
 print(type(my_string))
 print(type(my_number))
 print(type(my_dict))
+
+# a class is a template for a data type. it describes the info it will hold and how we can interact with it as engineers - capitalise the name of classes to make them easier to identify from variables 
+
+class BabysFirstClass:
+  pass
+
