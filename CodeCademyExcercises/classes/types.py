991b38e354cde6ff3efc8b04af855d5406e9e168
lottiejudge <lottielucyjudge@gmail.com>
2025-05-21 15:13:38 +0100

    instantiating first class

--- CodeCademyExcercises/classes/types.py
+++ CodeCademyExcercises/classes/types.py
@@ -8,8 +8,12 @@ print(type(my_string))
 print(type(my_number))
 print(type(my_dict))
 
-# a class is a template for a data type. it describes the info it will hold and how we can interact with it as engineers - capitalise the name of classes to make them easier to identify from variables 
+# a class is a template for a data type. it describes the info it will hold and how we can interact with it as engineers - capitalise the name of classes to make them easier to identify from variables. 
 
 class BabysFirstClass:
   pass
 
+# Classes must be instantiated to become alive, we must create an instance of it in order for it to become the schematic
+
+
+first_class = BabysFirstClass()
