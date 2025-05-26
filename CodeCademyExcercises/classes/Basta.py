7d2b4d856e2c553823f9aa9ae259cd8819b5e14e
lottiejudge <lottielucyjudge@gmail.com>
2025-05-26 20:46:53 +0100

    heck ye!

--- CodeCademyExcercises/classes/Basta.py
+++ CodeCademyExcercises/classes/Basta.py
@@ -3,7 +3,12 @@ class Menu:
     self.name = name
     self.items = items
     self.start_time = start_time
-    self.ent_time = end_time
+    self.end_time = end_time
+  def __repr__(self):
+    return "The {menu} is available from {start_time} to {end_time}".format(menu = self.name, start_time = self.start_time, end_time = self.end_time)
+  def calculate_bill(self, purchased_items):
+    return sum(purchased_items.values())
+  
 
 brunch = Menu('Brunch',{
   'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
@@ -16,3 +21,9 @@ early_bird = Menu('Early Bird', {
 dinner = Menu('Dinner', {
   'crostini with eggplant caponata': 13.00, 'caesar salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,
 }, 5, 11)
+
+kids = Menu('Kids', {
+  'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00
+}, 11, 9)
+
+print(brunch)
