505f26766f46ed01f89882c0f12e341e551318b2
lottiejudge <lottielucyjudge@gmail.com>
2025-07-21 21:23:20 +0100

    big o(1)

--- bigOnotation.py
+++ bigOnotation.py
@@ -1,2 +1,3 @@
-bigOis = 'Big O Notation refers to the time and space complexity of an algorithm/function. The more time and space a function takes. E.g Nested For Loops etc, the worse it is'
+bigOis = 'Big O Notation refers to the time and space complexity of an algorithm/function compared to the input. The more time and space a function takes. E.g Nested For Loops etc, the worse it is'
 
+O1 = 'Constant: regardless of the size of the input the execution time/space remains the same. This is the most efficient, in an ideal world all functions would be 0(1) baby'
