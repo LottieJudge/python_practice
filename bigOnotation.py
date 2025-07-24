3cd5e5e6b126567c6830657ed6575bb5439555b2
lottiejudge <lottielucyjudge@gmail.com>
2025-07-24 18:39:08 +0100

    O(n) Linear

--- bigOnotation.py
+++ bigOnotation.py
@@ -3,3 +3,5 @@ bigOis = 'Big O Notation refers to the time and space complexity of an algorithm
 O1 = ' 0(1) Constant: regardless of the size of the input the execution time/space remains the same. This is the most efficient, in an ideal world all functions would be 0(1) baby'
 
 OlogN = 'O(log n) Logarithmic complexity: if you needed to guess a number between 1 and 100, you would say 50 and someone says higher or lower, you have removed the original half of the incorrect answers. Where as if someone said just not right you have only removed one option(linear search 0(n)). 0(log n) is the one that removes half. e.g Binary Search'
+
+On = 'o(n) Linear: '
