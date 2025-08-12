ec78885ff7f3d6b11b482b828806a8a390e99cb2
lottiejudge <lottielucyjudge@gmail.com>
2025-08-12 20:26:46 +0100

    keyword arguments

--- functionArgumentNotes.py
+++ functionArgumentNotes.py
@@ -5,6 +5,13 @@ def printname(firstName, lastName):
 
 printname('Scott', 'Godfrey')
 
+keyWordArgument = 'Arguments that are called by their name, so within the function call assigning them data a bit like you would with a variable'
+
+def printdog(name, age):
+  print(name, age)
+
+printdog(name = 'Watson', age = 8)
+
 tables = {
   1: ['Jiho', False],
   2:[],
@@ -18,5 +25,8 @@ tables = {
 def assign_table(table_number, name, vip_status):
   tables[table_number] = [name, vip_status]
 
+# using positional aruguments in this 
 assign_table(9, 'Scott', True)
 print(tables)
+
+# using keyword aruguments to assign
