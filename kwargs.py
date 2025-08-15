ea60d38c240399fae1a0f9fb06c3d26ff1d8a08a
lottiejudge <lottielucyjudge@gmail.com>
2025-08-15 20:45:00 +0100

    kwaaaargs (keyword arguments)

---
+++ kwargs.py
@@ -0,0 +1,27 @@
+# KWAAAARGS == unlimited KEY WORD ARGUMENTS - not just args, we've got kwargs, two askerisks bb 
+
+tables = {
+  1: {
+    'name': 'Chioma',
+    'vip_status': False,
+    'order': {
+      'drinks': 'Orange Juice, Apple Juice',
+      'food_items': 'Pancakes'
+    }
+  },
+  2: {},
+  3: {},
+  4: {},
+  5: {},
+  6: {},
+  7: {},
+}
+
+
+
+
+# move print again for DRY principles 
+print(tables)
+
+
+
