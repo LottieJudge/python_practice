c4aee2348359055c217ac25abad63ca044a34d79
lottiejudge <lottielucyjudge@gmail.com>
2025-08-23 16:10:48 +0100

    unpacking operator

--- flashCards.py
+++ flashCards.py
@@ -1,3 +1,5 @@
+# Arguments
+
 NoneKeyword = 'The None Keyword is used as a workaround when defining empty lists/dictionaries as arguments. If you define an empty dataset within a function, every time you call that function it will call all previous iterations. So the None workaround defines the argument as none, which you then redefine as a data set within the function itself.'
 
 def noneKeyWord(age, type=None):
@@ -6,3 +8,16 @@ def noneKeyWord(age, type=None):
   print(age, type)
 
 noneKeyWord(4, 'Sweetcorn')
+
+unpackingOperator = 'The Unpacking operator (*) is used when you want to take in an unknown amount of arguments, they will then be returned as a single object'
+
+def unpackThis(*args):
+  print(args)
+
+unpackThis('Watson', 'Chewie', 'Harry', 'Kurgen', 'Tug', 'Ziggy', 'Misty')
+
+# the below will TypeError due to only one argument 
+def dontUnpackThis(args):
+  print(args)
+
+# dontUnpackThis('Watson', 'Chewie', 'Harry', 'Kurgen', 'Tug', 'Ziggy', 'Misty')
