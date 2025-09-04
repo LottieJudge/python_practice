a0183845ed276720281c0c06a508810dc39de993
lottiejudge <lottielucyjudge@gmail.com>
2025-09-04 12:59:54 +0100

    yaml to json learning

--- textManipulate.py
+++ textManipulate.py
@@ -1,5 +1,11 @@
 # text manipulation programme 
 # yaml to json 
+import yaml
+import json
 
-def yamlToJson(file):
-    
+osList = {}
+
+with open("./operating-systems.yml", 'r') as infile, open("python_operating-systems.json", 'w') as outfile:
+  yaml_object = yaml.safe_load(infile)
+  json.dump(infile, outfile)
+
