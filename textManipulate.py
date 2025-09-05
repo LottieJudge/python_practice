7dcf177e15884a65f1f946c1c86bb8c3f8f72047
lottiejudge <lottielucyjudge@gmail.com>
2025-09-05 17:37:26 +0100

    HUEHUEHUE

--- textManipulate.py
+++ textManipulate.py
@@ -7,5 +7,5 @@ osList = {}
 
 with open("./operating-systems.yml", 'r') as infile, open("python_operating-systems.json", 'w') as outfile:
   yaml_object = yaml.safe_load(infile)
-  json.dump(infile, outfile)
+  json.dump(yaml_object, outfile)
 
